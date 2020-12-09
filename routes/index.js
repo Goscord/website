01b6d6def57dd0502fcaf5af022cf94ba79cc2b1
Bluzzi <bluzzigame@gmail.com>
2020-12-09 21:41:10 +0100

    Pass current example code in index route

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
+const fs = require("fs");
+
 server.get("/", (request, response) => {
-    response.render("index");
+    let currentCode = fs.readFileSync(__dirname + "/../others/code/1.0.0.txt", {encoding: "utf-8"});
+    
+    response.render("index", {
+        currentCode: currentCode
+    });
 });
