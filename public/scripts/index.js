66703310aab4bccdd3c5f30f1fd6b8d78757dc86
Bluzzi <bluzzigame@gmail.com>
2020-12-09 21:42:41 +0100

    Add function copyText()

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,26 @@
+/**
+ * @param {HTMLElement} text 
+ */
+function copyText(text){
+    // Create text area and add text to content :
+    let area = document.createElement("textarea");
+
+    document.body.appendChild(area);
+
+    area.value = text.textContent;
+
+    // Select the text :
+    area.select();
+
+    // Copy it and remove text area :
+    document.execCommand("copy");
+    document.body.removeChild(area);
+
+    // Play animation on the text : 
+    text.style.animation = "colorTransition 3s";
+
+    setTimeout(() => text.style.removeProperty("animation"), 1000);
+}
 
 window.addEventListener("scroll", () => {
     let opacity = 1 - ((window.scrollY + 1) / 500);
