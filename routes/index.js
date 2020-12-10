3c0b37a0fb0a11181eb9f5c916cba2f640770035
Bluzzi <bluzzigame@gmail.com>
2020-12-10 13:32:56 +0100

    Change extention of code example

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 server.get("/", (request, response) => {
-    let currentCode = fs.readFileSync(__dirname + "/../others/code/1.0.0.txt", {encoding: "utf-8"});
+    let currentCode = fs.readFileSync(__dirname + "/../others/code/1.0.0.go", {encoding: "utf-8"});
     
     response.render("index", {
         currentCode: currentCode
