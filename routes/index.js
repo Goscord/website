07249a26a8140a9ae92a45cceb1ed2102d3addc7
szeroki <36236750+Seyz123@users.noreply.github.com>
2020-12-10 17:56:57 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ server.get("/", (request, response) => {
     let currentCode = fs.readFileSync(__dirname + "/../others/code/1.0.0.go", {encoding: "utf-8"});
     
     response.render("index", {
-        currentCode: currentCode
+        currentCode
     });
 });
