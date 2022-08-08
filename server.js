7d96fb1beefe89e8303e1c19c894bbbadebf8f4f
Bluzzi <camilledugas.pro@gmail.com>
2022-08-08 18:35:07 +0200

    Server: remove bodyParser and morgan (log)

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const fs = require("fs");
 const convertSass = require("sass-folder-converter");
 
@@ -9,9 +8,6 @@ convertSass(__dirname + "/public/sass/", __dirname + "/public/css/");
 // Create express instance :
 global.server = express();
 
-// Setup the logger :
-server.use(require("morgan")("dev"));
-
 // Define public folder : 
 server.use(express.static(__dirname + "/public"));
 
@@ -20,10 +16,6 @@ server.set("views", __dirname + "/views");
 
 // Setup view engine :
 server.set("view engine", "ejs");
-
-// Add body parser middleware for get body content (for post method) :
-server.use(bodyParser.json()); // support json encoded bodies
-server.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // Routes and 404 error :
 fs.readdirSync(__dirname + "/routes/").forEach(fileName => require("./routes/" + fileName)); //TODO: ajout du support des sous dossier.
