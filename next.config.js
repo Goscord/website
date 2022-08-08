1746f27f8d6d76444f12a839adf5eee55e9cb55a
Bluzzi <camilledugas.pro@gmail.com>
2022-08-08 21:14:40 +0200

    NextConfig: add redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/cRYXkCqDAj",
+        permanent: true
+      },
+      {
+        source: "/docs",
+        destination: "https://pkg.go.dev/github.com/Goscord/goscord#section-documentation",
+        permanent: false
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
