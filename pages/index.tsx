c1e56e44f8e0718c052cd61fc5b5179976bb213f
Bluzzi <camilledugas.pro@gmail.com>
2022-08-08 21:15:53 +0200

    /index (page): use getStaticProps() function for get example code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,18 @@ import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
 import { ReactElement } from "react";
 
+interface HomeProps {
+  exampleCode: string;
+}
+
+export function getStaticProps(): GetStaticPropsResult<HomeProps> {
+  return {
+    props: {
+      exampleCode: readFileSync(`${resolve()}/lib/examples/0-0-2.go`, { encoding: "utf-8" })
+    }
+  };
+}
+
 export default function Home({ exampleCode }: HomeProps): ReactElement {
   return (
     <p>Hello Goscord !</p>
