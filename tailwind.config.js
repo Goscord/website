80221c6d34944f9cffb49c0844e422c9a5ed48ec
Bluzzi <camilledugas.pro@gmail.com>
2022-08-08 22:28:47 +0200

    Tailwind: add colors, font size and screen size of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,38 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./lib/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: {
+        DEFAULT: "#5aa469",
+        light: "#72da7b"
+      },
+
+      black: {
+        DEFAULT: "black",
+        light: "#23272A"
+      }
+    },
+
+    fontSize: {
+      "base": "1rem",
+      "lg": "1.125rem",
+      "xl": "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": ["4.5rem", { lineHeight: 1 }]
+    },
+
+    screens: {
+      "2xl": { "max": "1535px" },
+      "xl": { "max": "1279px" },
+      "lg": { "max": "1023px" },
+      "md": { "max": "767px" },
+      "sm": { "max": "639px" }
+    },
+
     extend: {},
   },
   plugins: [],
