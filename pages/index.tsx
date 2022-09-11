445442cbe58aacf34aa692bf5e75860187123118
Bluzzi <camilledugas.pro@gmail.com>
2022-09-11 15:10:35 +0200

    / (page): test button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticPropsResult } from "next";
 import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
 import { ReactElement } from "react";
+import Button from "$lib/components/navigation/Button";
 
 interface HomeProps {
   exampleCode: string;
@@ -17,6 +18,8 @@ export function getStaticProps(): GetStaticPropsResult<HomeProps> {
 
 export default function Home({ exampleCode }: HomeProps): ReactElement {
   return (
-    <p>Hello Goscord !</p>
+    <div className="h-screen bg-black-light grid place-content-center">
+      <Button size="large">Test</Button>
+    </div>
   );
 }
