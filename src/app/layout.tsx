3b0b65c00a514def31ea19f4d50116a8b40b1714
Bluzzi <camilledugas.pro@gmail.com>
2023-02-17 00:56:49 +0100

    work in progress...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,26 @@
+"use client";
+
+import "#/lib/styles/reset.css";
+import "#/lib/styles/font.css";
 import "#/lib/styles/tailwind.css";
+import { Navbar } from "#/lib/components/navbar/navbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, ReactElement } from "react";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({ children }: PropsWithChildren): ReactElement {
   return (
     <html lang="en">
       <head />
 
-      <body>{children}</body>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          <Navbar />
+
+          {children}
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
 import { ReactElement } from "react";
+import { Text } from "#/lib/components/text";
+import Image from "next/image";
 
 export default function Home(): ReactElement {
   return (
     <>
+      <header className="bg-gray py-36 flex justify-center items-center flex-col gap-14">
+        <div className="relative w-10/12 h-44">
+          <Image src="/goscord-brand.png" alt="Goscord brand" fill />
+        </div>
 
+        <Text size="3xl">The best Discord API in Golang.</Text>
+      </header>
     </>
   );
 }
