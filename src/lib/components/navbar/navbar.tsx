f786cfb4458fd573d9435a12fab6e3911b816b28
Bluzzi <camilledugas.pro@gmail.com>
2023-02-17 19:58:06 +0100

    navbar (component): add responsive support

--- src/lib/components/navbar/navbar.tsx
+++ src/lib/components/navbar/navbar.tsx
@@ -1,20 +1,40 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { navbarPages } from "#/lib/configs/navbar";
 import { NavbarItem } from "./navbav-item";
 import { Theme } from "#/lib/components/theme";
-import { GitHubStars } from "../github-stars";
+import { GitHubStars } from "#/lib/components/github-stars";
+import { Burger } from "#/lib/components/burger";
+import { clsx } from "clsx";
 
 export function Navbar(): ReactElement {
+  const [burgerOpen, setBurgerOpen] = useState(false);
+
   return (
-    <nav className="bg-primary h-20 flex items-center justify-between px-16">
+    <nav className={clsx(
+      "bg-primary h-96 max-h-20 flex items-center justify-between px-16 sm:px-3",
+      "transition-[max-height] duration-300",
+      {
+        "max-h-96 flex-col justify-evenly gap-5": burgerOpen
+      }
+    )}>
       <GitHubStars />
 
-      <div className="flex items-center gap-10">
+      <div className={clsx(
+        "flex items-center",
+        {
+          "gap-10 lg:hidden": !burgerOpen,
+          "flex-col gap-2": burgerOpen
+        }
+      )}>
         {navbarPages.map(page => (
           <NavbarItem key={page.label} label={page.label} href={page.href} external={page.external} />
         ))}
 
+      </div>
+
+      <div className="flex items-center gap-2">
         <Theme />
+        <Burger open={burgerOpen} setOpen={setBurgerOpen} className="hidden lg:block" />
       </div>
     </nav>
   );
