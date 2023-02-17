a1fcb45d3e4374ab2c04aee57b0f27103aeac0e0
Bluzzi <camilledugas.pro@gmail.com>
2023-02-17 15:16:24 +0100

    navbar (component): use the new composants

--- src/lib/components/navbar/navbar.tsx
+++ src/lib/components/navbar/navbar.tsx
@@ -1,30 +1,20 @@
-import { ReactElement, useState } from "react";
-import { FiGithub, FiExternalLink, FiSun, FiMoon } from "react-icons/fi";
-import { useQuery } from "@tanstack/react-query";
-import { getGithubStars } from "#/lib/utils/github";
-import { Text } from "#/lib/components/text";
-import { goscord } from "#/lib/configs/goscord";
+import { ReactElement } from "react";
+import { navbarPages } from "#/lib/configs/navbar";
+import { NavbarItem } from "./navbav-item";
+import { Theme } from "#/lib/components/theme";
+import { GitHubStars } from "../github-stars";
 
 export function Navbar(): ReactElement {
-  const { isSuccess: githubStarsReady, data: githubStars } = useQuery(["github", "stars"], getGithubStars);
-  const [theme, setTheme] = useState<"light" | "dark">("light");
-
   return (
     <nav className="bg-primary h-20 flex items-center justify-between px-16">
-      {/* GitHub Stars */}
-      <Text size="large" className="flex items-center gap-2">
-        <FiGithub color="white" />
-        <span>{githubStarsReady ? githubStars : "..."}</span> STARS AND COUNTING
-      </Text>
+      <GitHubStars />
 
       <div className="flex items-center gap-10">
-        <Text size="large" className="bg-primary-1 py-3 px-4 rounded">{goscord.webiste.domain}</Text>
-        <Text size="large">Documentation</Text>
-        <Text size="large" className="flex items-center gap-2">GitHub <FiExternalLink /></Text>
+        {navbarPages.map(page => (
+          <NavbarItem key={page.label} label={page.label} href={page.href} external={page.external} />
+        ))}
 
-        <Text size="2xl" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-          {theme === "light" ? <FiSun /> : <FiMoon />}
-        </Text>
+        <Theme />
       </div>
     </nav>
   );
