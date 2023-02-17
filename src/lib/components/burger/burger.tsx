09efd53a8c6ba8ef5225b1eabce861e2f604ba51
Bluzzi <camilledugas.pro@gmail.com>
2023-02-17 20:13:15 +0100

    burger (component): close the burger when user navigate to a new page

--- src/lib/components/burger/burger.tsx
+++ src/lib/components/burger/burger.tsx
@@ -1,9 +1,18 @@
 import { clsx } from "clsx";
-import { ReactElement } from "react";
+import { usePathname } from "next/navigation";
+import { ReactElement, useEffect } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { BurgerProps } from "./burger.type";
 
 export function Burger({ open, setOpen, className, ...props }: BurgerProps): ReactElement {
+  // Close the burger menu when the user navigates to a new page:
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setOpen(false);
+  }, [pathname, setOpen]);
+
+  // Render the burger menu:
   const Tag = open ? FiX : FiMenu;
 
   return (
