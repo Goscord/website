e3bf29c63cf554773ebcf92ae82364672b9a8655
Bluzzi <camilledugas.pro@gmail.com>
2023-02-17 15:16:09 +0100

    navbar-item (component): create this component

---
+++ src/lib/components/navbar/navbav-item.tsx
@@ -0,0 +1,32 @@
+import { ReactElement } from "react";
+import { NavbarItemProps } from "./navbar.type";
+import { FiExternalLink } from "react-icons/fi";
+import { Text } from "#/lib/components/text";
+import { clsx } from "clsx";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export function NavbarItem({ label, href, external = false }: NavbarItemProps): ReactElement {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  console.log(pathname, href, isActive);
+
+  return (
+    <Link href={href} target={external ? "_blank" : "_self"}>
+      <Text size="large" className={clsx(
+        "py-3 px-4 rounded",
+        "",
+        "transition-colors duration-300",
+        {
+          "flex items-center gap-2": external,
+          "bg-primary-1": isActive,
+          "bg-transparent hover:bg-primary-1": !isActive
+        }
+      )}>
+        {label}
+        {external && <FiExternalLink />}
+      </Text>
+    </Link>
+  );
+}
