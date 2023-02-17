ccc1b1006fdef06fbcfc6def50e037d94cb9e1db
Bluzzi <camilledugas.pro@gmail.com>
2023-02-17 19:44:03 +0100

    github-stars (component): add link

--- src/lib/components/github-stars/github-stars.tsx
+++ src/lib/components/github-stars/github-stars.tsx
@@ -3,15 +3,19 @@ import { Text } from "#/lib/components/text";
 import { useQuery } from "@tanstack/react-query";
 import { getGithubStars } from "#/lib/utils/github";
 import { FiGithub } from "react-icons/fi";
+import { goscord } from "#/lib/configs/goscord";
+import Link from "next/link";
 
 export function GitHubStars(): ReactElement {
   const { isSuccess: githubStarsReady, data: githubStars } = useQuery(["github", "stars"], getGithubStars);
 
   return (
-    <Text size="large" className="flex items-center gap-2">
-      <FiGithub />
+    <Link href={goscord.github.mainRepoUrl} target="_blank">
+      <Text size="large" className="flex items-center gap-2">
+        <FiGithub />
 
-      <span>{githubStarsReady ? githubStars : "..."}</span> STARS AND COUNTING
-    </Text>
+        <span>{githubStarsReady ? githubStars : "..."}</span> STARS AND COUNTING
+      </Text>
+    </Link>
   );
 }
