aabce88044e5a49ae277b4eb129999171e26b8da
Bluzzi <camilledugas.pro@gmail.com>
2023-02-17 15:27:11 +0100

    navbar (config): fix GitHub page url

--- src/lib/configs/navbar/navbar.config.ts
+++ src/lib/configs/navbar/navbar.config.ts
@@ -4,5 +4,5 @@ import { NavbarItem } from "./navbar.type";
 export const navbarPages: NavbarItem[] = [
   { label: goscord.webiste.domain, href: "/" },
   { label: "Documentation", href: "/documentation" },
-  { label: "GitHub", href: "/github", external: true }
+  { label: "GitHub", href: goscord.github.mainRepoUrl, external: true }
 ];
