dfceb2fd265aceeaab4144bd05e65a86928eb782
Bluzzi <camilledugas.pro@gmail.com>
2023-02-17 14:29:01 +0100

    theme (store): add `switchTheme()` function

--- src/lib/stores/theme/theme.store.ts
+++ src/lib/stores/theme/theme.store.ts
@@ -3,5 +3,6 @@ import { ThemeStore } from "./theme.type";
 
 export const useThemeStore = create<ThemeStore>((set) => ({
   theme: "light",
-  setTheme: theme => set({ theme })
+  setTheme: theme => set({ theme }),
+  switchTheme: () => set(state => ({ theme: state.theme === "light" ? "dark" : "light" }))
 }));
