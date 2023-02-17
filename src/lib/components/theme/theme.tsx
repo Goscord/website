71e540de4ec472cb9202ea3f939629c6ee5a0177
Bluzzi <camilledugas.pro@gmail.com>
2023-02-17 15:25:11 +0100

    theme (icon): add background on hover

--- src/lib/components/theme/theme.tsx
+++ src/lib/components/theme/theme.tsx
@@ -2,12 +2,17 @@ import { ReactElement } from "react";
 import { Text } from "#/lib/components/text";
 import { FiSun, FiMoon } from "react-icons/fi";
 import { useThemeStore } from "#/lib/stores/theme";
+import { clsx } from "clsx";
 
 export function Theme(): ReactElement {
   const { theme, switchTheme } = useThemeStore();
 
   return (
-    <Text size="2xl" onClick={() => switchTheme()}>
+    <Text size="2xl" onClick={() => switchTheme()} className={clsx(
+      "p-4 rounded cursor-pointer",
+      "transition-colors duration-300",
+      "hover:bg-primary-1"
+    )}>
       {theme === "light" ? <FiSun /> : <FiMoon />}
     </Text>
   );
