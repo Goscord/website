ad7658ef1b8bc4641a4c9420ac2f76582ab6b0be
Bluzzi <camilledugas.pro@gmail.com>
2023-02-18 19:50:33 +0100

    / (page): add lorem just for tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,22 @@ export default function Home(): ReactElement {
 
         <Text size="3xl">The best Discord API in Golang.</Text>
       </header>
+
+      <section className="flex flex-col items-center gap-5 w-10/12 mx-auto my-10">
+        {Array.from({ length: 20 }).map((_, i) => (
+          <Text key={i} color="black">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Qui porro exercitationem soluta placeat hic, voluptates, ex magnam, animi vero est earum.
+            Nihil, dolore. Facere perferendis quod obcaecati iure, fuga autem?
+
+            {i % 2 === 0 && `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Qui porro exercitationem soluta placeat hic, voluptates, ex magnam, animi vero est earum.
+              Nihil, dolore. Facere perferendis quod obcaecati iure, fuga autem?
+            `}
+          </Text>
+        ))}
+      </section>
     </>
   );
 }
