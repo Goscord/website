08f9493ff95de5d47889a8d1921157bec177f81e
Bluzzi <camilledugas.pro@gmail.com>
2023-02-18 20:04:52 +0100

    navbar (component): splite renders for better responsive mangement

--- src/lib/components/navbar/navbar.tsx
+++ src/lib/components/navbar/navbar.tsx
@@ -5,39 +5,59 @@ import { Theme } from "#/lib/components/theme";
 import { GitHubStars } from "#/lib/components/github-stars";
 import { Burger } from "#/lib/components/burger";
 import { clsx } from "clsx";
+import { useMediaQuery } from "#/lib/hooks/media-query";
 
 export function Navbar(): ReactElement {
   const [burgerOpen, setBurgerOpen] = useState(false);
+  const isMobile = useMediaQuery({ type: "max", width: "1023px" });
 
-  return (
-    <nav className={clsx(
-      "bg-primary h-96 max-h-20",
-      "flex items-center justify-between",
-      "px-16 sm:px-3",
-      "transition-[max-height] duration-300",
-      {
-        "max-h-96 flex-col justify-evenly gap-5": burgerOpen
-      }
-    )}>
-      <GitHubStars />
+  if (isMobile) return (
+    <nav className="bg-primary px-16 sm:px-3">
+      {/* Menu */}
+      <div className="h-20 flex items-center justify-between">
+        <GitHubStars />
 
+        <div className="flex items-center gap-2">
+          <Theme />
+          <Burger open={burgerOpen} setOpen={setBurgerOpen} />
+        </div>
+      </div>
+
+      {/* Pages and links */}
       <div className={clsx(
-        "flex items-center",
+        "h-96 flex flex-col gap-3",
+        "transition-[max-height] duration-300",
         {
-          "gap-10 lg:hidden": !burgerOpen,
-          "place-content-end w-full mr-10": !burgerOpen,
-          "flex-col gap-2": burgerOpen
+          "max-h-96": burgerOpen,
+          "max-h-0 overflow-hidden": !burgerOpen
         }
       )}>
+        {navbarPages.map(page => (
+          <NavbarItem
+            key={page.label}
+            label={page.label}
+            href={page.href}
+            external={page.external}
+            className={clsx(
+              "transition-opacity duration-300",
+              !burgerOpen && "opacity-0",
+            )}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+
+  return (
+    <nav className="bg-primary h-20 flex items-center justify-between px-16">
+      <GitHubStars />
+
+      <div className="flex items-center gap-10">
         {navbarPages.map(page => (
           <NavbarItem key={page.label} label={page.label} href={page.href} external={page.external} />
         ))}
 
-      </div>
-
-      <div className="flex items-center gap-2">
         <Theme />
-        <Burger open={burgerOpen} setOpen={setBurgerOpen} className="hidden lg:block" />
       </div>
     </nav>
   );
