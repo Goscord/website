22a4baa7a34518a695ae7387dc73b246b05e9e65
Bluzzi <camilledugas.pro@gmail.com>
2023-02-20 03:20:35 +0100

    / (layout): split providers

---
+++ src/app/provider.tsx
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren, ReactElement } from "react";
+
+const queryClient = new QueryClient();
+
+export function Provider({ children }: PropsWithChildren): ReactElement {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
