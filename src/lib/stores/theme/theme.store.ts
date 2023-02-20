309b396679a41b4e7f511f3cb155748c8f179680
Bluzzi <camilledugas.pro@gmail.com>
2023-02-20 02:39:58 +0100

    theme (store): persist in local storage

--- src/lib/stores/theme/theme.store.ts
+++ src/lib/stores/theme/theme.store.ts
@@ -1,8 +1,17 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { ThemeStore } from "./theme.type";
 
-export const useThemeStore = create<ThemeStore>((set) => ({
-  theme: "light",
-  setTheme: theme => set({ theme }),
-  switchTheme: () => set(state => ({ theme: state.theme === "light" ? "dark" : "light" }))
-}));
+export const useThemeStore = create(
+  persist<ThemeStore>(
+    set => ({
+      theme: "light",
+      setTheme: theme => set({ theme }),
+      switchTheme: () => set(state => ({
+        theme: state.theme === "light" ? "dark" : "light"
+      }))
+    }),
+
+    { name: "theme" }
+  )
+);
