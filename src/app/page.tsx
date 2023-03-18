deb2d761d2073077b43f8de8f891f4c493a22b0d
Bluzzi <camilledugas.pro@gmail.com>
2023-03-18 21:51:59 +0100

    / (page): create header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 import { ReactElement } from "react";
 import { Text } from "#/lib/components/text";
-import Image from "next/image";
+import { HeaderSection } from "./header.section";
 
 export default function Home(): ReactElement {
   return (
     <>
-      <header className="bg-gray py-36 flex justify-center items-center flex-col gap-14">
-        <div className="relative w-10/12 h-44">
-          <Image src="/goscord-brand.png" alt="Goscord brand" fill />
-        </div>
-
-        <Text size="3xl">The best Discord API in Golang.</Text>
-      </header>
+      <HeaderSection />
 
       <section className="flex flex-col items-center gap-5 w-10/12 mx-auto my-10">
         {Array.from({ length: 20 }).map((_, i) => (
