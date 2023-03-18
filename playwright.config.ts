2dfb60dac23a88d7c08c58df8a9d602874d953cd
Bluzzi <camilledugas.pro@gmail.com>
2023-03-18 15:40:32 +0100

    playwright (config): divers fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 import "dotenv/config";
 
 // Server base URL and port:
@@ -8,12 +8,36 @@ const baseURL = `http://localhost:${port}`;
 // Playwright config:
 export default defineConfig({
   testDir: "./test",
-  testMatch: "*.ts",
 
   timeout: 30 * 1000,
 
+  reporter: [["html", { open: "always" }]],
+
   webServer: {
     command: "npm run dev",
-    url: baseURL
-  }
+    url: baseURL,
+    reuseExistingServer: !process.env.CI
+  },
+
+  use: {
+    baseURL: baseURL,
+    trace: "retry-with-trace"
+  },
+
+  projects: [
+    {
+      name: "desktop",
+      use: {
+        ...devices["Desktop Chrome"]
+      },
+      testMatch: /.*\/[A-z]+\.(desktop\.)?ts/
+    },
+    {
+      name: "mobile",
+      use: {
+        ...devices["iPhone XR"]
+      },
+      testMatch: /.*\/[A-z]+\.(mobile\.)?ts/
+    }
+  ]
 });
