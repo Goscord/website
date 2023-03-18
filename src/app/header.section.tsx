e18a10e52f2744613ebc184df0144e0056126aec
Bluzzi <camilledugas.pro@gmail.com>
2023-03-18 21:51:59 +0100

    / (page): create header section

---
+++ src/app/header.section.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from "react";
+import { Button } from "#/lib/components/button";
+import { FiCopy } from "react-icons/fi";
+import { Text } from "#/lib/components/text";
+import Image from "next/image";
+import goscordBrand from "~/goscord-brand.png";
+
+export function HeaderSection(): ReactElement {
+  return (
+    <header className="bg-gray py-20 flex justify-center items-center flex-col gap-5">
+      <Image src={goscordBrand} alt="Goscord brand" className="sm:w-11/12 w-10/12 max-w-5xl" />
+
+      <Text size="2xl">A powerful Discord API in Golang.</Text>
+
+      <Button size="large" icon={<FiCopy />}>go get github.com/Goscord/goscord</Button>
+    </header>
+  );
+}
