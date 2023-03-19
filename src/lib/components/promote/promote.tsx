5203e3f2677652a573a9f1e0b33b33599eec27dc
Bluzzi <camilledugas.pro@gmail.com>
2023-03-19 17:34:54 +0100

    promote (component): improve responsive

--- src/lib/components/promote/promote.tsx
+++ src/lib/components/promote/promote.tsx
@@ -4,8 +4,8 @@ import { PromoteProps } from "./promote.type";
 
 export function Promote({ icon, title, description }: PromoteProps): ReactElement {
   return (
-    <div className="w-96">
-      <Text color="black" size="2xl" className="flex items-center gap-2">{icon} {title}</Text>
+    <div className="w-96 px-3 max-w-full sm:text-center">
+      <Text color="black" size="2xl" className="flex items-center gap-2 sm:justify-center">{icon} {title}</Text>
 
       <Text color="black">{description}</Text>
     </div>
