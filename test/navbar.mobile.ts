9077be259209a3ca9c32543936936a7f30f8cee5
Bluzzi <camilledugas.pro@gmail.com>
2023-03-19 22:54:17 +0100

    navbar (test/mobile): fix

--- test/navbar.mobile.ts
+++ test/navbar.mobile.ts
@@ -7,8 +7,15 @@ test("click on navbar items", async({ page }) => {
   // Open burger menu:
   await page.click("#burger");
 
-  // Check if all navbar pages are visible:
+  // Check if navbar page button are visible and have the right link:
   for (const item of navbarPages) {
-    expect(page.getByText(item.label)).toBeVisible();
+    const link = page.getByRole("link", { name: item.label });
+
+    // const href = await link.getAttribute("href");
+    // const target = await link.getAttribute("target");
+
+    expect(link).toBeVisible();
+    // expect(href).toBe(item.href);
+    // expect(target).toBe(item.external ? "_blank" : "_self");
   }
 });
