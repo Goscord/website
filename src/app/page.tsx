aac16f15f68f734e57d31a8b332b18fd1f02929f
Bluzzi <camilledugas.pro@gmail.com>
2023-03-19 04:04:55 +0100

    / (page): create about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import { ReactElement } from "react";
 import { Text } from "#/lib/components/text";
 import { HeaderSection } from "./header.section";
+import { AboutSection } from "./about.section";
 
 export default function Home(): ReactElement {
   return (
     <>
       <HeaderSection />
 
-      <section className="flex flex-col items-center gap-5 w-10/12 mx-auto my-10">
+      <AboutSection />
+
+      <section className="flex flex-col gap-5 p-16 bg-gray">
         {Array.from({ length: 20 }).map((_, i) => (
-          <Text key={i} color="black">
+          <Text key={i} color="white">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Qui porro exercitationem soluta placeat hic, voluptates, ex magnam, animi vero est earum.
             Nihil, dolore. Facere perferendis quod obcaecati iure, fuga autem?
