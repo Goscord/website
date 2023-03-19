cfab892cfc59259a350ba4f27018fb79cfb02a20
Bluzzi <camilledugas.pro@gmail.com>
2023-03-19 19:22:00 +0100

    code (component): create this component

---
+++ src/lib/components/atoms/code/code.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { ReactElement, useEffect } from "react";
+import { CodeProps } from "./code.type";
+import { FiCopy } from "react-icons/fi";
+import { Tooltip } from "../tooltip";
+import { toast } from "sonner";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/components/prism-go";
+
+export function Code({ code, language }: CodeProps): ReactElement {
+  const copyToClipboard = (): void => {
+    navigator.clipboard.writeText(code);
+
+    toast.success("Copied to clipboard!");
+  };
+
+  useEffect(() => {
+    Prism.highlightAll();
+  });
+
+  return (
+    <div className="rounded shadow relative">
+      <span className="absolute top-4 right-4">
+        <Tooltip text="Copy to clipboard!">
+          <FiCopy className="text-white text-xl brightness-75 hover:brightness-100 active:scale-95" onClick={() => copyToClipboard()} />
+        </Tooltip>
+      </span>
+
+      <pre>
+        <code className={`language-${language}`}>{code}</code>
+      </pre>
+    </div>
+  );
+}
---
+++ src/lib/components/atoms/code/code.type.ts
@@ -0,0 +1,4 @@
+export type CodeProps = {
+  language: "html" | "xml" | "svg" | "css" | "go" | "javascript";
+  code: string;
+};
