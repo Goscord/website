d5ebf57f1debadec76e6ed8102ab8395fb684bd5
Bluzzi <camilledugas.pro@gmail.com>
2023-03-19 19:40:09 +0100

    tooltip (component): add block container

--- src/lib/components/atoms/tooltip/tooltip.tsx
+++ src/lib/components/atoms/tooltip/tooltip.tsx
@@ -5,20 +5,22 @@ import { Text } from "#/lib/components/atoms/text";
 
 export function Tooltip({ text, children }: TooltipProps): ReactElement {
   return (
-    <HoverCard.Root>
-      <HoverCard.Trigger>
-        <div className="mb-2">{children}</div>
-      </HoverCard.Trigger>
+    <div>
+      <HoverCard.Root>
+        <HoverCard.Trigger>
+          <div>{children}</div>
+        </HoverCard.Trigger>
 
-      <HoverCard.Portal>
-        <HoverCard.Content>
-          <HoverCard.Arrow />
+        <HoverCard.Portal>
+          <HoverCard.Content>
+            <HoverCard.Arrow />
 
-          <div className="bg-black rounded p-5 py-2">
-            <Text color="white">{text}</Text>
-          </div>
-        </HoverCard.Content>
-      </HoverCard.Portal>
-    </HoverCard.Root>
+            <div className="bg-black rounded p-5 py-2">
+              <Text color="white">{text}</Text>
+            </div>
+          </HoverCard.Content>
+        </HoverCard.Portal>
+      </HoverCard.Root>
+    </div>
   );
 }
