01fa39a0fac789023075ccdd386eb7731542c28c
Bluzzi <camilledugas.pro@gmail.com>
2023-03-19 03:45:32 +0100

    / (page): copy "go got" to clipboard and notify this

--- src/app/header.section.tsx
+++ src/app/header.section.tsx
@@ -5,10 +5,18 @@ import { Button } from "#/lib/components/button";
 import { FiCopy } from "react-icons/fi";
 import { Text } from "#/lib/components/text";
 import { Tooltip } from "#/lib/components/tooltip";
+import { goscord } from "#/lib/configs/goscord";
+import { toast } from "sonner";
 import Image from "next/image";
 import goscordBrand from "~/goscord-brand.png";
 
 export function HeaderSection(): ReactElement {
+  const copyToClipboard = (): void => {
+    navigator.clipboard.writeText(goscord.github.install);
+
+    toast.success("Command copied to clipboard!");
+  };
+
   return (
     <header className="bg-gray py-20 flex justify-center items-center flex-col gap-5">
       <Image src={goscordBrand} alt="Goscord brand" className="sm:w-11/12 w-10/12 max-w-5xl" />
@@ -16,7 +24,9 @@ export function HeaderSection(): ReactElement {
       <Text size="2xl">A powerful Discord API in Golang.</Text>
 
       <Tooltip text="Copy to clipboard!">
-        <Button size="large" icon={<FiCopy />}>go get github.com/Goscord/goscord</Button>
+        <Button size="large" icon={<FiCopy />} onClick={() => copyToClipboard()}>
+          {goscord.github.install}
+        </Button>
       </Tooltip>
     </header>
   );
