b96ccc733fbf38920a531a26ba80fe567c86ea09
Bluzzi <camilledugas.pro@gmail.com>
2023-03-20 15:59:50 +0100

    / (page): improve responsive of setup section

--- src/app/setup.section.tsx
+++ src/app/setup.section.tsx
@@ -6,20 +6,20 @@ import { ReactElement } from "react";
 
 export function SetupSection(): ReactElement {
   return (
-    <section className="bg-gray grid grid-cols-2 bg-setup-section bg-cover">
-      <div className="py-28 mx-auto w-3/4 space-y-4">
+    <section className="bg-gray bg-setup-section bg-cover py-12 px-5 flex justify-center gap-5 flex-wrap">
+      <div className="py-16 space-y-6 max-w-xl mx-auto">
         <Text size="2xl">How to Set Up?</Text>
         <Text size="3xl">You can use the <strong className="text-primary">Goscord</strong> package by copying this:</Text>
 
-        <GoGetButton />
+        <GoGetButton size="large" />
 
-        <Text size="large">
+        <Text size="xl">
           That{"'"}s it, now you can copy the following example and start creating your Discord bot!
           You can also discover more possibilities on our documentation.
         </Text>
       </div>
 
-      <div className="p-10 opacity-90">
+      <div className="opacity-90 max-w-2xl w-full mx-auto">
         <Code code={codeExample} language="go" />
       </div>
     </section>
