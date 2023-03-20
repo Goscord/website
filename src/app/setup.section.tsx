df3f9c85f009ea0c22f8ef45c3e34ba523a9f8e9
Bluzzi <camilledugas.pro@gmail.com>
2023-03-20 18:06:38 +0100

    / (page): finish setup section

--- src/app/setup.section.tsx
+++ src/app/setup.section.tsx
@@ -1,8 +1,11 @@
+import { LinkButton } from "#/lib/components/atoms/button";
 import { Code } from "#/lib/components/atoms/code";
 import { Text } from "#/lib/components/atoms/text";
 import { GoGetButton } from "#/lib/components/molecules/go-get-button";
 import { codeExample } from "#/lib/configs/code-example";
 import { ReactElement } from "react";
+import { BsDiscord } from "react-icons/bs";
+import { FiBook } from "react-icons/fi";
 
 export function SetupSection(): ReactElement {
   return (
@@ -11,12 +14,21 @@ export function SetupSection(): ReactElement {
         <Text size="2xl">How to Set Up?</Text>
         <Text size="3xl">You can use the <strong className="text-primary">Goscord</strong> package by copying this:</Text>
 
-        <GoGetButton size="large" />
+        <GoGetButton />
 
         <Text size="xl">
           That{"'"}s it, now you can copy the following example and start creating your Discord bot!
           You can also discover more possibilities on our documentation.
         </Text>
+
+        <LinkButton href="/documentation" icon={<FiBook />}>See documentation</LinkButton>
+
+        <Text size="xl">
+          Join our Discord community to connect with other Goscord users and developers.
+          Click the Discord button below to join our server and get help from our friendly members.
+        </Text>
+
+        <LinkButton href="/discord" target="_blank" icon={<BsDiscord />}>Join our Discord</LinkButton>
       </div>
 
       <div className="opacity-90 max-w-2xl w-full mx-auto">
