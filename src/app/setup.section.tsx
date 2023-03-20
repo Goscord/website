253eec7ce1653cba7b07e8b7607e58f38fcfddb9
Bluzzi <camilledugas.pro@gmail.com>
2023-03-20 19:26:56 +0100

    / (page): add better separator for setup section

--- src/app/setup.section.tsx
+++ src/app/setup.section.tsx
@@ -3,37 +3,46 @@ import { Code } from "#/lib/components/atoms/code";
 import { Text } from "#/lib/components/atoms/text";
 import { GoGetButton } from "#/lib/components/molecules/go-get-button";
 import { codeExample } from "#/lib/configs/code-example";
+import { clsx } from "clsx";
 import { ReactElement } from "react";
 import { BsDiscord } from "react-icons/bs";
 import { FiBook } from "react-icons/fi";
 
 export function SetupSection(): ReactElement {
   return (
-    <section className="bg-gray bg-setup-section bg-cover py-12 px-5 flex justify-center gap-5 flex-wrap">
-      <div className="py-16 space-y-6 max-w-xl mx-auto">
-        <Text size="2xl">How to Set Up?</Text>
-        <Text size="3xl">You can use the <strong className="text-primary">Goscord</strong> package by copying this:</Text>
+    <section className="w-full overflow-x-hidden">
+      <div className="bg-gray bg-setup-section bg-cover py-12 px-5 flex justify-center gap-5 flex-wrap">
+        <div className="py-16 space-y-6 max-w-xl mx-auto">
+          <Text size="2xl">How to Set Up?</Text>
+          <Text size="3xl">You can use the <strong className="text-primary">Goscord</strong> package by copying this:</Text>
 
-        <GoGetButton />
+          <GoGetButton />
 
-        <Text size="xl">
+          <Text size="xl">
           That{"'"}s it, now you can copy the following example and start creating your Discord bot!
           You can also discover more possibilities on our documentation.
-        </Text>
+          </Text>
 
-        <LinkButton href="/documentation" icon={<FiBook />}>See documentation</LinkButton>
+          <LinkButton href="/documentation" icon={<FiBook />}>See documentation</LinkButton>
 
-        <Text size="xl">
+          <Text size="xl">
           Join our Discord community to connect with other Goscord users and developers.
           Click the Discord button below to join our server and get help from our friendly members.
-        </Text>
+          </Text>
 
-        <LinkButton href="/discord" target="_blank" icon={<BsDiscord />}>Join our Discord</LinkButton>
+          <LinkButton href="/discord" target="_blank" icon={<BsDiscord />}>Join our Discord</LinkButton>
+        </div>
+
+        <div className="opacity-90 max-w-2xl w-full mx-auto">
+          <Code code={codeExample} language="go" />
+        </div>
       </div>
 
-      <div className="opacity-90 max-w-2xl w-full mx-auto">
-        <Code code={codeExample} language="go" />
-      </div>
+      <div className={clsx(
+        "border-l-[100vw] border-l-transparent",
+        "border-b-[100px] border-b-transparent",
+        "border-t-[100px] border-t-gray"
+      )} />
     </section>
   );
 }
