e0e20f674feb9c49e526538b34e1f802ac2601dd
Bluzzi <camilledugas.pro@gmail.com>
2023-03-29 05:23:29 +0200

    / (page): use SSR for getting stats

--- src/app/statistic.section.tsx
+++ src/app/statistic.section.tsx
@@ -1,25 +1,21 @@
-"use client";
-
 import { Text } from "#/lib/components/atoms/text";
 import { Statistic } from "#/lib/components/molecules/statistic";
-import { getGithubClone, getGithubStars } from "#/lib/utils/github";
-import { useQuery } from "@tanstack/react-query";
+import { goscord } from "#/lib/configs/goscord";
+import { getGitHubStars, getGitHubClone } from "#/lib/utils/github";
 import { ReactElement } from "react";
 import { FiBarChart, FiGithub } from "react-icons/fi";
 
-export function StatisticSection(): ReactElement {
-  const { data } = useQuery(["github", "stats"], async() => ({
-    starsCount: await getGithubStars(),
-    cloneCount: await getGithubClone()
-  }));
+export async function StatisticSection(): Promise<ReactElement> {
+  const starsCount = await getGitHubStars(goscord.github.owner, goscord.github.repo);
+  const cloneCount = await getGitHubClone(goscord.github.owner, goscord.github.repo);
 
   return (
     <section className="flex items-center justify-center flex-col gap-8">
       <Text color="primary" size="3xl">Want some Goscord stats?</Text>
 
       <div className="flex items-center gap-20 flex-wrap">
-        <Statistic icon={<FiGithub />} text="GitHub stars" count={data?.starsCount} />
-        <Statistic icon={<FiBarChart />} text="Downloads" count={data?.cloneCount} />
+        <Statistic icon={<FiGithub />} text="GitHub stars" count={starsCount} />
+        <Statistic icon={<FiBarChart />} text="Downloads" count={cloneCount} />
       </div>
     </section>
   );
