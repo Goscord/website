19271ffb0e6d85689627a2e6f37dfa21ff63d453
Bluzzi <camilledugas.pro@gmail.com>
2023-03-29 22:24:37 +0200

    declaration: fix png import

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,9 +1,11 @@
+import type { StaticImageData } from "next/image";
+
 declare module "*.css" {
   const content: Record<string, string>;
   export default content;
 }
 
 declare module "*.png" {
-  const content: string;
+  const content: StaticImageData;
   export default content;
 }
