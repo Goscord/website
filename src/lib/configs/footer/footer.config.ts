1a4062c4e5b05e3ba8f628664bd133354f60874a
Bluzzi <camilledugas.pro@gmail.com>
2023-03-30 23:00:36 +0200

    footer (config): add footer sections

--- src/lib/configs/footer/footer.config.ts
+++ src/lib/configs/footer/footer.config.ts
@@ -10,5 +10,21 @@ export const footerLinks: FooterLinks[] = [
       { name: "Example bot", link: goscord.github.botRepoUrl },
       { name: "Backend", link: goscord.github.backendRepoUrl }
     ]
+  },
+  {
+    title: "Resources",
+    links: [
+      { name: "Branding", link: "/branding" },
+      { name: "Community", link: "/discord" },
+      { name: "Report a problem", link: `${goscord.github.mainRepoUrl}/issues` }
+    ]
+  },
+  {
+    title: "Developers",
+    links: [
+      { name: "Documentation", link: "/documentation" },
+      { name: "GitHub", link: goscord.github.organizationUrl },
+      { name: "Changelog", link: `${goscord.github.mainRepoUrl}/releases` }
+    ]
   }
 ];
