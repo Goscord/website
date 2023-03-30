94384946add9eaa9562f15e7dba9b08a8b20ae1c
Bluzzi <camilledugas.pro@gmail.com>
2023-03-30 04:42:32 +0200

    / (page): improve statistic responsive

--- src/app/statistic.section.tsx
+++ src/app/statistic.section.tsx
@@ -4,19 +4,27 @@ import { Statistic } from "#/lib/components/molecules/statistic";
 import { goscord } from "#/lib/configs/goscord";
 import { getGitHubStars, getGitHubClone } from "#/lib/utils/github";
 import { FiBarChart, FiGithub } from "react-icons/fi";
+import Image from "next/image";
+import triangleShape from "~/shapes/gray-triangle.png";
 
 export async function StatisticSection(): Promise<ReactElement> {
   const starsCount = await getGitHubStars(goscord.github.owner, goscord.github.repo);
   const cloneCount = await getGitHubClone(goscord.github.owner, goscord.github.repo);
 
   return (
-    <section className="flex items-center justify-center flex-col gap-8 mt-5 mb-14">
-      <Text color="primary" size="3xl" className="font-bold">Want some Goscord stats?</Text>
+    <>
+      <Image src={triangleShape} alt="triangle shape" />
 
-      <div className="flex items-center gap-20 flex-wrap">
-        <Statistic icon={<FiGithub />} text="GitHub stars" count={starsCount} />
-        <Statistic icon={<FiBarChart />} text="Downloads" count={cloneCount} />
-      </div>
-    </section>
+      <section className="flex items-center justify-center flex-col gap-10 my-14">
+        <Text color="primary" size="3xl" className="font-bold text-center">Want some Goscord stats?</Text>
+
+        <div className="flex items-center justify-center gap-20 sm:gap-8 flex-wrap">
+          <Statistic icon={<FiGithub />} text="GitHub stars" count={starsCount} />
+          <Statistic icon={<FiBarChart />} text="Downloads" count={cloneCount} />
+        </div>
+      </section>
+
+      <Image src={triangleShape} alt="triangle shape" className="rotate-180" />
+    </>
   );
 }
