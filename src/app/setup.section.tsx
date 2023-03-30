95f393a347c6e5945f804f5089e17cf09bd058ea
Bluzzi <camilledugas.pro@gmail.com>
2023-03-30 04:37:47 +0200

    / (page): remove shape from setup section

--- src/app/setup.section.tsx
+++ src/app/setup.section.tsx
@@ -6,40 +6,34 @@ import { codeExample } from "#/lib/configs/code-example";
 import { ReactElement } from "react";
 import { BsDiscord } from "react-icons/bs";
 import { FiBook } from "react-icons/fi";
-import Image from "next/image";
-import triangleShape from "~/shapes/gray-triangle.png";
 
 export function SetupSection(): ReactElement {
   return (
-    <section className="w-full overflow-x-hidden">
-      <div className="bg-gray bg-setup-section bg-cover py-12 px-5 flex justify-center gap-5 flex-wrap">
-        <div className="py-16 space-y-6 max-w-xl mx-auto">
-          <Text size="2xl">How to Set Up?</Text>
-          <Text size="3xl">You can use the <strong className="text-primary">Goscord</strong> package by copying this:</Text>
+    <section className="bg-gray bg-setup-section bg-cover py-12 px-5 flex justify-center gap-5 flex-wrap">
+      <div className="py-16 space-y-6 max-w-xl mx-auto">
+        <Text size="2xl">How to Set Up?</Text>
+        <Text size="3xl">You can use the <strong className="text-primary">Goscord</strong> package by copying this:</Text>
 
-          <GoGetButton />
+        <GoGetButton />
 
-          <Text size="xl">
+        <Text size="xl">
             That{"'"}s it, now you can copy the following example and start creating your Discord bot!
             You can also discover more possibilities on our documentation.
-          </Text>
+        </Text>
 
-          <LinkButton href="/documentation" icon={<FiBook />}>See documentation</LinkButton>
+        <LinkButton href="/documentation" icon={<FiBook />}>See documentation</LinkButton>
 
-          <Text size="xl">
+        <Text size="xl">
             Join our Discord community to connect with other Goscord users and developers.
             Click the Discord button below to join our server and get help from our friendly members.
-          </Text>
+        </Text>
 
-          <LinkButton href="/discord" target="_blank" icon={<BsDiscord />}>Join our Discord</LinkButton>
-        </div>
-
-        <div className="opacity-90 max-w-2xl w-full mx-auto">
-          <Code code={codeExample} language="go" />
-        </div>
+        <LinkButton href="/discord" target="_blank" icon={<BsDiscord />}>Join our Discord</LinkButton>
       </div>
 
-      <Image src={triangleShape} alt="triangle shape" className="w-full" />
+      <div className="opacity-90 max-w-2xl w-full mx-auto">
+        <Code code={codeExample} language="go" />
+      </div>
     </section>
   );
 }
