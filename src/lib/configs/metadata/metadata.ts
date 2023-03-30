3aea20d139c602f353fd37ad35b595197eac7eba
Bluzzi <camilledugas.pro@gmail.com>
2023-03-30 04:25:58 +0200

    metadata (config): create this config

---
+++ src/lib/configs/metadata/metadata.ts
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Goscord - A Discord API wrapper written in Golang",
+  description: [
+    "Goscord is a Discord bot creation API written in the Go programming language.",
+    "It offers a high level API rich in possibilities and which evolves at the same pace as Discord."
+  ].join(" "),
+
+  themeColor: "#5aa469",
+
+  icons: "/goscord.png"
+};
