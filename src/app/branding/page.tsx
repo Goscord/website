f98a254518629e1d1b904b3590c6afb54a3742f8
Bluzzi <camilledugas.pro@gmail.com>
2023-03-31 00:01:00 +0200

    /branding (page): use default export for nextjs routing

--- src/app/branding/page.tsx
+++ src/app/branding/page.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from "react";
 import { Text } from "#/lib/components/atoms/text";
 
-export function Branding(): ReactElement {
+export default function Branding(): ReactElement {
   return (
     <div className="flex items-center justify-center flex-col gap-5 h-96">
       <Text color="black">Coming soon...</Text>
