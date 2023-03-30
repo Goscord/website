2756fb11dd90dff5f7e2a70fa5b1fe529fe478b4
Bluzzi <camilledugas.pro@gmail.com>
2023-03-30 04:34:32 +0200

    / (layout): use font & metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,16 @@
 import "#/lib/styles/reset.css";
-import "#/lib/styles/font.css";
 import "#/lib/styles/tailwind.css";
 import { Navbar } from "#/lib/components/layouts/navbar";
 import { PropsWithChildren, ReactElement } from "react";
 import { Provider } from "./provider";
-import { Metadata } from "next";
+import { metadata } from "#/lib/configs/metadata";
+import { inter, robotoMono } from "#/lib/configs/font";
 
-export const metadata: Metadata = {
-  title: "Goscord - A Discord API wrapper written in Golang",
-  description: [
-    "Goscord is a Discord bot creation API written in the Go programming language.",
-    "It offers a high level API rich in possibilities and which evolves at the same pace as Discord."
-  ].join(" "),
-
-  themeColor: "#5aa469",
-
-  icons: "/goscord.png"
-};
+export { metadata };
 
 export default function RootLayout({ children }: PropsWithChildren): ReactElement {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${robotoMono.variable}`}>
       <head />
 
       <body>
