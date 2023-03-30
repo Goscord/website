ce0895d6949fbc1256d38f8cb78c2980c726b1c6
Bluzzi <camilledugas.pro@gmail.com>
2023-03-30 23:56:34 +0200

    / (page): finish footer section

--- src/app/footer.section.tsx
+++ src/app/footer.section.tsx
@@ -6,20 +6,21 @@ import { clsx } from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import goscordLogo from "~/goscord.png";
+import { footerLinks } from "#/lib/configs/footer";
 
 export function FooterSection(): ReactElement {
   const mediaStyles = "text-gray-2 cursor-pointer hover:brightness-125 active:scale-95";
 
   return (
-    <footer className="bg-gray py-8 flex items-center justify-center">
+    <footer className="w-full bg-gray pt-8 pb-16 px-4 flex gap-16 justify-around items-center lg:flex-col-reverse">
       <div className="space-y-3">
-        <Text color="gray-2" className="flex gap-1 items-center">
+        <Text color="gray-2" className="flex gap-1 items-center justify-center flex-wrap">
           <Image src={goscordLogo} alt="goscord logo" className="h-5 w-5 rounded" />
           <strong className="text-primary font-normal">Goscord</strong>
           - Made with ❤️ by the Goscord team
         </Text>
 
-        <div className="flex items-center gap-8">
+        <div className="flex items-center gap-8 sm:justify-center">
           <Link href={goscord.github.mainRepoUrl} target="_blank">
             <BsGithub className={clsx("text-xl", mediaStyles)} />
           </Link>
@@ -30,8 +31,24 @@ export function FooterSection(): ReactElement {
         </div>
       </div>
 
-      <div>
+      <div className="flex gap-28 sm:flex-col">
+        {footerLinks.map(group => (
+          <div key={group.title} className="space-y-3">
+            <Text color="gray-3">{group.title}</Text>
 
+            {group.links.map(link => (
+              <Text
+                key={link.name}
+                color="gray-2"
+                className="hover:underline hover:brightness-125 cursor-pointer"
+              >
+                <Link href={link.link} target="_blank">
+                  {link.name}
+                </Link>
+              </Text>
+            ))}
+          </div>
+        ))}
       </div>
     </footer>
   );
