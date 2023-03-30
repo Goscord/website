7d413b8008d1deb1fd183b065a95dbac7db91d5b
Bluzzi <camilledugas.pro@gmail.com>
2023-03-30 06:01:13 +0200

    goscord (config): add links of all repositories

--- src/lib/configs/goscord/goscord.ts
+++ src/lib/configs/goscord/goscord.ts
@@ -10,6 +10,9 @@ export const goscord = {
     owner: "Goscord",
     repo: "goscord",
     mainRepoUrl: "https://github.com/Goscord/goscord",
+    websiteRepoUrl: "https://github.com/Goscord/website",
+    botRepoUrl: "https://github.com/Goscord/bot",
+    backendRepoUrl: "https://github.com/Goscord/backend",
     install: "go get github.com/Goscord/goscord"
   }
 };
