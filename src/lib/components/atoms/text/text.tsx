78be321fd4fba7bae09f86c80c476dbc8f69880a
Bluzzi <camilledugas.pro@gmail.com>
2023-03-30 05:29:24 +0200

    text (component): add gray 2 variant

--- src/lib/components/atoms/text/text.tsx
+++ src/lib/components/atoms/text/text.tsx
@@ -8,6 +8,7 @@ export function Text({ children, type, color, size, className, ...props }: TextP
     "text-white": !color || color === "white",
     "text-black": color === "black",
     "text-gray": color === "gray",
+    "text-gray-2": color === "gray-2",
     "text-primary": color === "primary",
 
     // Font size:
--- src/lib/components/atoms/text/text.type.ts
+++ src/lib/components/atoms/text/text.type.ts
@@ -2,6 +2,6 @@ import { HTMLAttributes, PropsWithChildren } from "react";
 
 export type TextProps = PropsWithChildren & HTMLAttributes<HTMLParagraphElement> & {
   type?: "p" | "h3" | "h2" | "h1";
-  color?: "white" | "black" | "gray" | "primary";
+  color?: "white" | "black" | "gray" | "gray-2" | "primary";
   size?: "small" | "base" | "large" | "xl" | "2xl" | "3xl";
 }
