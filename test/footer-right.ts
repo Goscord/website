7b50f4eb6572602c3ecdaa991929bc72bca54ee3
Bluzzi <camilledugas.pro@gmail.com>
2023-04-02 11:00:53 +0200

    footer-right (test/e2e): test footer right sections

---
+++ test/footer-right.ts
@@ -0,0 +1,28 @@
+import { footerLinks } from "#/lib/configs/footer";
+import { test, expect } from "@playwright/test";
+
+test("footer right sections", async({ page }) => {
+  await page.goto("/");
+
+  const footer = page.locator("footer");
+
+  for (const section of footerLinks) {
+    const title = footer.getByRole("heading", { name: section.title });
+
+    expect(title).toBeVisible();
+
+    for (const item of section.links) {
+      const link = footer.getByRole("link", { name: item.name });
+
+      expect(link).toBeVisible();
+
+      await link.click();
+
+      const href = await link.getAttribute("href");
+      const target = await link.getAttribute("target");
+
+      expect(href).toBe(item.link);
+      expect(target).toBe("_blank");
+    }
+  }
+});
