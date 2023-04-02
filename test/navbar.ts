7772c02fedbabdf9b1bbfa687cfe7a068afa70cc
Bluzzi <camilledugas.pro@gmail.com>
2023-04-02 10:18:27 +0200

    navbar (test/e2e): test click, visible, responsive, and href content

---
+++ test/navbar.ts
@@ -0,0 +1,28 @@
+import { navbarPages } from "#/lib/configs/navbar";
+import { test, expect, devices } from "@playwright/test";
+
+test("navbar elements", async({ page }) => {
+  await page.goto("/");
+
+  // Open burger menu (if is mobile device):
+  if (page.viewportSize()?.width === devices["iPhone XR"].viewport.width) {
+    await page.click("#burger");
+  }
+
+  // Check if navbar page button are visible and have the right link:
+  const navbar = page.getByRole("navigation");
+
+  for (const item of navbarPages) {
+    const link = navbar.getByRole("link", { name: item.label });
+
+    expect(link).toBeVisible();
+
+    await link.click();
+
+    const href = await link.getAttribute("href");
+    const target = await link.getAttribute("target");
+
+    expect(href).toBe(item.href);
+    expect(target).toBe(item.external ? "_blank" : "_self");
+  }
+});
