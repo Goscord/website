ba77bbb18237b442a992aa67cab50ebc77ceb8a5
Bluzzi <camilledugas.pro@gmail.com>
2023-04-02 09:44:31 +0200

    /dcoumentation (page): improve responsive

--- src/app/documentation/page.tsx
+++ src/app/documentation/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Documentation(): ReactElement {
   return (
     <div className="flex items-center justify-center flex-col gap-5 h-96">
-      <Text color="black">Our documentation coming soon...</Text>
+      <Text color="black" className="text-center">Our documentation coming soon...</Text>
 
       <Link href="https://pkg.go.dev/github.com/Goscord/goscord#section-documentation" target="_blank">
         <Text color="primary" className="hover:underline">While waiting, you can use the Golang documentation.</Text>
