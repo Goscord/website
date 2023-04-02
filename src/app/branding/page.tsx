4680ec375c06243b7f164e0569af9857dc2a90e9
Bluzzi <camilledugas.pro@gmail.com>
2023-04-02 09:44:24 +0200

    /branding (page): improve responsive

--- src/app/branding/page.tsx
+++ src/app/branding/page.tsx
@@ -4,7 +4,7 @@ import { Text } from "#/lib/components/atoms/text";
 export default function Branding(): ReactElement {
   return (
     <div className="flex items-center justify-center flex-col gap-5 h-96">
-      <Text color="black">Coming soon...</Text>
+      <Text color="black" className="text-center">Coming soon...</Text>
     </div>
   );
 }
