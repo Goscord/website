935fe5822d728c38f5a77aeb7a3e41ae213df84a
Bluzzi <camilledugas.pro@gmail.com>
2023-04-03 00:12:17 +0200

    go-get-button (component): use the new copy to clipboard function

--- src/lib/components/molecules/go-get-button/go-get-button.tsx
+++ src/lib/components/molecules/go-get-button/go-get-button.tsx
@@ -7,17 +7,18 @@ import { FiCopy } from "react-icons/fi";
 import { goscord } from "#/lib/configs/goscord";
 import { toast } from "sonner";
 import { clsx } from "clsx";
+import { copyToClipboard } from "#/lib/utils/clipboard";
 
 export function GoGetButton({ className, ...props }: ButtonProps): ReactElement {
-  const copyToClipboard = (): void => {
-    navigator.clipboard.writeText(goscord.github.install);
+  const copyText = async(): Promise<void> => {
+    copyToClipboard(goscord.github.install);
 
     toast.success("Command copied to clipboard!");
   };
 
   return (
     <Tooltip text="Copy to clipboard!">
-      <Button icon={<FiCopy />} onClick={() => copyToClipboard()} className={clsx("font-mono", className)} {...props}>
+      <Button icon={<FiCopy />} onClick={() => copyText()} className={clsx("font-mono", className)} {...props}>
         {goscord.github.install}
       </Button>
     </Tooltip>
