5b4df38ca7e34726c8a754d3ed506433b9ab6e3e
Bluzzi <camilledugas.pro@gmail.com>
2023-04-04 17:07:42 +0200

    footer-left (test/e2e): split tests

--- test/footer-left.ts
+++ test/footer-left.ts
@@ -1,35 +1,40 @@
 import { goscord } from "#/lib/configs/goscord";
 import { test, expect } from "@playwright/test";
 
-test("footer left link icons", async({ page }) => {
-  await page.goto("/");
+test.describe("footer left link icons", () => {
+  test("github icon", async({ page }) => {
+    await page.goto("/");
 
-  // Footer locator:
-  const footer = page.locator("footer");
+    const footer = page.locator("footer");
 
-  // GitHub icon:
-  const github = footer.locator("#github-link-icon");
-  const githubAttributes = {
-    href: await github.getAttribute("href"),
-    target: await github.getAttribute("target")
-  };
+    const github = footer.locator("#github-link-icon");
+    const githubAttributes = {
+      href: await github.getAttribute("href"),
+      target: await github.getAttribute("target")
+    };
 
-  expect(github).toBeVisible();
-  expect(githubAttributes.href).toBe(goscord.github.mainRepoUrl);
-  expect(githubAttributes.target).toBe("_blank");
+    expect(github).toBeVisible();
+    expect(githubAttributes.href).toBe(goscord.github.mainRepoUrl);
+    expect(githubAttributes.target).toBe("_blank");
 
-  await github.click();
+    await github.click();
+  });
 
-  // Discord icon:
-  const discord = footer.locator("#discord-link-icon");
-  const discordAttributes = {
-    href: await discord.getAttribute("href"),
-    target: await discord.getAttribute("target")
-  };
+  test("discord icon", async({ page }) => {
+    await page.goto("/");
 
-  expect(discord).toBeVisible();
-  expect(discordAttributes.href).toBe(goscord.discord.url);
-  expect(discordAttributes.target).toBe("_blank");
+    const footer = page.locator("footer");
 
-  await discord.click();
+    const discord = footer.locator("#discord-link-icon");
+    const discordAttributes = {
+      href: await discord.getAttribute("href"),
+      target: await discord.getAttribute("target")
+    };
+
+    expect(discord).toBeVisible();
+    expect(discordAttributes.href).toBe(goscord.discord.url);
+    expect(discordAttributes.target).toBe("_blank");
+
+    await discord.click();
+  });
 });
